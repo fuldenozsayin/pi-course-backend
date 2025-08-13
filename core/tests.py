82bd5524cb9c4c05f71df4f41593ac806130dde0
Fulden Özsayın <201902048@ogrenci.amasya.edu.tr>
2025-08-13 18:21:35 +0300

    Backend

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+# core/tests.py
+from django.test import TestCase
+class SmokeTest(TestCase):
+    def test_truth(self):
+        self.assertTrue(True)
